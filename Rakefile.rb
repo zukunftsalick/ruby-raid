6fbecbfc7bc38d9d1aa33736e581eca52dbfdfb8
Rubem Nakamura Carneiro <rubem.nakamura@gmail.com>
2010-06-30 15:01:29 -0300

    Adding Rakefile - generating rdoc

---
+++ Rakefile.rb
@@ -0,0 +1,11 @@
+require 'rake'
+require 'rake/testtask'
+require 'rake/rdoctask'
+
+
+#Task para gerar a documentacao automaticamente
+Rake::RDocTask.new do |rd|
+   rd.main = "README.rdoc"
+   rd.rdoc_files.include("README.rdoc", "enemies/**/*.rb", "train.rb", "game_window.rb")
+end
+
